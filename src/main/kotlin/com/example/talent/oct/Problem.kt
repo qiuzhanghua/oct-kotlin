8df25db1833d4819153a718bb9e282ae8f114738
Daniel <qiuzhanghua@icloud.com>
2020-08-29 15:08:04 +0800

    O(n)/O(1)

--- src/main/kotlin/com/example/talent/oct/Problem.kt
+++ src/main/kotlin/com/example/talent/oct/Problem.kt
@@ -4,13 +4,16 @@ class Problem {
     fun collect(coins: IntArray): Int {
         val n = coins.size
         if (n < 2) return 0
-        val dp = IntArray(n)
-        dp[1] = coins[1]
-        for (i in 0 until n - 2) {
-            dp[i + 2] = Math.max(dp[i + 2], dp[i] + coins[i + 2])
-            if (i + 3 < n) dp[i + 3] = Math.max(dp[i + 3], dp[i] + coins[i + 3])
+        if (n < 3) return coins[1]
+        var f0 = 0;
+        var f1 = coins[1]
+        var f2 = Math.max(coins[1], coins[2])
+        for (i in 3 until n) {
+            var f3 = Math.max(f0, f1) + coins[i]
+            f0 = f1
+            f1 = f2
+            f2 = f3
         }
-//        println(dp.joinToString(",", "[", "]"))
-        return Math.max(dp[n - 1], dp[n - 2])
+        return Math.max(f1, f2)
     }
 }
